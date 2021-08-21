007f74d00e922bdd85cfe11e33039d1c2af753ca
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-08-21 15:17:18 +0300

    add form component

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,12 +1,12 @@
 export class Quote {
     showDetail: boolean
     postDate!: Date
-    upVote!: number
-    downVote!: number
+    upVote: number
+    downVote: number
     constructor(public quote: string, public author: string, public publisher: string) {
         this.showDetail = false
         this.postDate = new Date()
-        this.upVote = 0
-        this.downVote = 0
+        this.upVote= 0
+        this.downVote= 0
     }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Quote } from '../quote';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[]=[
+      new Quote('Quote ni wewe ', 'Mandela', 'Me'),
+      new Quote('Quote ya pili', 'HAjulikani', 'NI mimi')
+  ]
 
   constructor() { }
 
