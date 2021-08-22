7ce7514f89bf56861dcb386cdc036cc91eb92396
CheboiDerrick <derrick.cheboi@student.moringaschool.com>
2021-08-22 11:06:26 +0300

    add delete event to the quote detail component

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-detail',
@@ -6,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-detail.component.css']
 })
 export class QuoteDetailComponent implements OnInit {
+  @Input () quote!:Quote;
+  @Output () isDeleteQuote=new EventEmitter<boolean>();
+
+  quoteDelete(remove:boolean){
+    this.isDeleteQuote.emit(remove);
+  }
 
   constructor() { }
 
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,8 +10,21 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]=[
-      new Quote('The stars are for only those who can reach them', 'HAjulikani', 'NI mimi')
+      new Quote('The stars are for only those who can reach them', 'Nelson Mandela', 'Andy Taylor')
   ]
+
+  toggleDetails(index:number){
+    this.quotes[index].showDetail = !this.quotes[index].showDetail;
+  }
+
+  deleteQuote(isDeleteQuote:boolean, index:number){
+    if (isDeleteQuote) {
+      let toDelete= confirm(`This will remove ${this.quotes[index].publisher}'s quote. Proceed?`)
+      if (toDelete) {
+        this.quotes.splice(index,1)
+      }
+    }
+  }
 
   constructor() { }
 
